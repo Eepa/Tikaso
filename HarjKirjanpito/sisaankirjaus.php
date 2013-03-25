8427810d83df3b3627c7976b83dc952d94130de4
Eveliina Pakarinen <eve.pakarinen@gmail.com>
2013-03-25 15:20:31 +0200

    vikajatkuu

--- HarjKirjanpito/sisaankirjaus.php
+++ HarjKirjanpito/sisaankirjaus.php
@@ -14,7 +14,7 @@
         <div> 
             <h3>Kirjaudu järjestelmään</h3>
 
-            <form action="kirjautuminen.php?sisaan" method="post">
+            <form action="kirjautuminen.php" method="post">
                 <p>Syötä tunnuksesi ja salasanasi:</p>
                 <fieldset>
                     
