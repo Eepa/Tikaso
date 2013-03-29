6c6ea2040bb632202d6be592fd624ea119ac5ce9
Eveliina Pakarinen <pevetiainen.ep@hotmail.com>
2013-03-29 11:57:50 +0200

    Linkkilistaparannus

--- HarjKirjanpito/linkkilista.php
+++ HarjKirjanpito/linkkilista.php
@@ -6,6 +6,21 @@
 
     </head>
     <body>
+        <script language="JavaScript">
+            document.addEventListener("DOMContentLoaded", function() {
+                // dokumentin KAIKKI a-elementit
+                linkit = document.getElementsByTagName("a");
+
+                for (var a = 0; a < linkit.length; a++) {
+                    if (document.location.href.match(linkit[a].href)) {
+                        // a-elementti ilman href-attribuuttia on validi
+                        linkit[a].removeAttribute("href");
+                    }
+                }
+            }, false);
+    
+        </script>
+
         <?php if (on_kirjautunut()) { ?>
             <p>
                 <a href="kirjaudu.php?ulos">Uloskirjautuminen</a>
